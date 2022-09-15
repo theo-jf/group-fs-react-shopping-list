526c4dbab2868f4337c18f618c8522e57e20ed43
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-15 16:45:54 -0500

    Added server.js necessities

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
+import ItemForm from '../ItemForm/ItemForm.jsx';
 
 // PUT GET IN HERE :)
-
 function App() {
+    let [itemList, setItemList] = useState([]);
+
+    useEffect(() => {
+        getItems();
+    }, []);
+
+    const getItems = () => {
+        
+    }
+
     return (
         <div className="App">
             <Header />
